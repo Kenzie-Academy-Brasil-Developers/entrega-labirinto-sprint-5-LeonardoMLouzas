6d8f760fdc58e49be4b61f132f9bfdf8667413ae
leomlouzas <leonardomlouzas@gmail.com>
2021-09-30 19:24:59 -0300

    termino do funcionamento do jogo

---
+++ assets/js/mapa.js
@@ -0,0 +1,63 @@
+/*
+INFORMAÇÕES PARA CRIAR O TABLUEIRO:
+
+"W" = Parede
+"S" = Início
+"F" = Chegada
+" " = Caminho
+
+*/
+
+// const map = [
+//     "WWWWWWWWWWWWWWWWWWWWW",
+//     "W   W     W     W W W",
+//     "W W W WWW WWWWW W W W",
+//     "W W W   W     W W   W",
+//     "W WWWWWWW W WWW W W W",
+//     "W         W     W W W",
+//     "W WWW WWWWW WWWWW W W",
+//     "W W   W   W W     W W",
+//     "W WWWWW W W W WWW W F",
+//     "S     W W W W W W WWW",
+//     "WWWWW W W W W W W W W",
+//     "W     W W W   W W W W",
+//     "W WWWWWWW WWWWW W W W",
+//     "W       W       W   W",
+//     "WWWWWWWWWWWWWWWWWWWWW",
+// ];
+
+// const map = [
+//     "WWWWWWWWWWWWWWWWWWWWW",
+//     "W   W     W     W W W",
+//     "W W W WWW WWWWW W W W",
+//     "W W W   W     W W   W",
+//     "W WWWWWWW W WWW W W W",
+//     "W         W     W W W",
+//     "W WWW WWWWW WWWWW W W",
+//     "W W   W   W W     W W",
+//     "W WWWWW W W W WWW W S",
+//     "F     W W W W W W WWW",
+//     "WWWWW W W W W W W W W",
+//     "W     W W W   W W W W",
+//     "W WWWWWWW WWWWW W W W",
+//     "W       W       W   W",
+//     "WWWWWWWWWWWWWWWWWWWWW",
+// ];
+
+const map = [
+    "WWWWWWWWWWWWWWWWWWWSW",
+    "W   W     W     W W W",
+    "W W W WWW WWWWW W W W",
+    "W W W   W     W W   W",
+    "W WWWWWWW W WWW W W W",
+    "W         W     W W W",
+    "W WWW WWWWW WWWWW W W",
+    "W W   W   W W     W W",
+    "W WWWWW W W W WWW W W",
+    "W     W W W W W W WWW",
+    "WWWWW W W W W W W W W",
+    "W     W W W   W W W W",
+    "W WWWWWWW WWWWW W W W",
+    "W       W       W   W",
+    "WFWWWWWWWWWWWWWWWWWWW",
+];
